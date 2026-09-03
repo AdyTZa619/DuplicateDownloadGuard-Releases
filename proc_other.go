e98f48fdc157f98eab3058d3d7eff19e8402984f
AdyTZa619 <137645268+AdyTZa619@users.noreply.github.com>
2026-09-04 02:08:27 +0300

    Keep non-Windows process wait stub in sync

--- proc_other.go
+++ proc_other.go
@@ -2,8 +2,13 @@
 
 package main
 
-import "os/exec"
+import (
+	"os/exec"
+	"time"
+)
 
 func hideChildWindow(cmd *exec.Cmd) {}
 
 func detachUpdaterProcess(cmd *exec.Cmd) {}
+
+func waitForProcessExit(pid int, timeout time.Duration) bool { return true }
