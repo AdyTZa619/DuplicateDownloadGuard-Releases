0351921d9984f5bc4ff9fa7e286d8a272ac02d66
AdyTZa619 <137645268+AdyTZa619@users.noreply.github.com>
2026-09-15 13:49:28 +0300

    CineCalendar: use global precision calendar engine

--- CineCalendar/cinecalendar/service.py
+++ CineCalendar/cinecalendar/service.py
@@ -5,7 +5,7 @@ from .autoseed import ensure_initial_ratings
 from .calendar_engine_v2 import RichCalendarEngine
 from .db import Database
 from .logging_setup import setup_logging
-from .recommender_v6 import FastRecommendationEngineV6
+from .recommender_v7 import FastRecommendationEngineV7
 from .util import AppPaths
 
 
@@ -17,7 +17,7 @@ class CineCalendarService:
         self._defaults()
         self.initial_ratings_state = ensure_initial_ratings(self.db, self.paths.root.parent, self.log)
         self.calendar = RichCalendarEngine()
-        self.recommender = FastRecommendationEngineV6(self.db, self.calendar)
+        self.recommender = FastRecommendationEngineV7(self.db, self.calendar)
 
     def _defaults(self):
         if self.db.get_setting("exclude_romance", None) is None:
